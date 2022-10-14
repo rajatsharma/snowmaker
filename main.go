6abdb6ca23e89b56cd06374603656f3b98d1f876
Rajat Sharma <lunasunkaiser@gmail.com>
2022-10-15 03:45:34 +0530

    create template for flake.nix

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"fmt"
 	"os"
 	"path"
+	"text/template"
 )
 
 func relativePath(file string) string {
@@ -28,26 +31,58 @@ func exists(path string) bool {
 	panic(fmt.Sprintf("error while reading %s occured: %v", path, err))
 }
 
+//go:embed flake.txt
+var flake string
+
+type Flake struct {
+	language string
+}
+
 func main() {
+	var language string
 	if exists(relativePath("stack.yaml")) {
 		println("haskell project found")
-		return
+		language = "haskell"
 	}
 
 	if exists(relativePath("go.mod")) {
 		println("go project found")
-		return
+		language = "go"
 	}
 
 	if exists(relativePath("Cargo.toml")) {
 		println("rust project found")
-		return
+		language = "rust"
 	}
 
 	if exists(relativePath("package.json")) && exists(relativePath("pnpm-lock.yaml")) {
 		println("node pnpm project found")
-		return
+		language = "node"
 	}
 
-	println("unknown project")
+	if language == "" {
+		println("unsupported language found")
+		os.Exit(1)
+	}
+
+	tmpl, err := template.New("nix").Parse(flake)
+
+	if err != nil {
+		panic("unable to parse template")
+	}
+
+	buf := new(bytes.Buffer)
+	tmpl.Execute(buf, Flake{language: language})
+
+	err = os.WriteFile(relativePath("flake.nix"), []byte(buf.String()), 0644)
+
+	if err != nil {
+		panic("unable to write flake.nix")
+	}
+
+	err = os.WriteFile(relativePath(".envrc"), []byte("use flake"), 0644)
+
+	if err != nil {
+		panic("unable to write .envrc")
+	}
 }
